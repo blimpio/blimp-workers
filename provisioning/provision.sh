83f81c13b654d807c754ab377dd098c941a2537f
Giovanni Collazo <hello@gcollazo.com>
2014-03-07 13:25:44 -0400

    Revert "Remove custom ssh port"
    This reverts commit b9d9b18171db66752863cce5675baaf114773499.

--- provisioning/provision.sh
+++ provisioning/provision.sh
@@ -12,12 +12,12 @@ LOGFILE=~/vagrant_provision.log
 # Start provisioning log
 echo '`date +%H:%M:%S` : Start provision.sh' >> $LOGFILE
 
-wget -qO- https://raw.github.com/GetBlimp/blimp-workers/master/provisioning/setup.sh
+wget -qO- https://raw.github.com/GetBlimp/blimp-workers/master/provisioning/setup.sh | bash -s $9
 wget -qO- https://raw.github.com/GetBlimp/blimp-workers/master/provisioning/memcached.sh | bash -s $1 $2 $3
 wget -qO- https://raw.github.com/GetBlimp/blimp-workers/master/provisioning/redis.sh | bash -s $4 $5
 wget -qO- https://raw.github.com/GetBlimp/blimp-workers/master/provisioning/python.sh | bash
 wget -qO- https://raw.github.com/GetBlimp/blimp-workers/master/provisioning/templates.sh
-wget -qO- https://raw.github.com/GetBlimp/blimp-workers/master/provisioning/firewall.sh | bash -s $3 $5 $7
+wget -qO- https://raw.github.com/GetBlimp/blimp-workers/master/provisioning/firewall.sh | bash -s $3 $5 $7 $9
 
 # Finished provisioning log
 echo '`date +%H:%M:%S` : Finished provision.sh' >> $LOGFILE
