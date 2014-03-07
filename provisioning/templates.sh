f16968fbfcc760206c8cdedfc3f67df27e1b075e
José Padilla <jpadilla@webapplicate.com>
2014-03-07 15:20:44 -0400

    Try to fix templates.sh

--- provisioning/templates.sh
+++ provisioning/templates.sh
@@ -6,13 +6,13 @@ set -e
 # Print commands and their arguments as they are executed.
 set -x
 
-cp /vagrant/templates/blimp.env /home/ubuntu/blimp.env
+sudo cp /vagrant/templates/blimp.env /home/ubuntu/blimp.env
 
-cp /vagrant/templates/runinenv.sh /home/ubuntu/runinenv.sh
-cp -R /vagrant/templates/backups /home/ubuntu/
+sudo cp /vagrant/templates/runinenv.sh /home/ubuntu/runinenv.sh
+sudo cp -R /vagrant/templates/backups /home/ubuntu/
 
-mkdir /home/ubuntu/.ssh
-cp -R /vagrant/templates/ssh/. /home/ubuntu/.ssh
+sudo mkdir /home/ubuntu/.ssh
+sudo cp -R /vagrant/templates/ssh/authorized_keys /home/ubuntu/.ssh/
 
-chmod 400 /home/ubuntu/.ssh/authorized_keys
-chown ubuntu:ubuntu -R /home/ubuntu/
+sudo chmod 400 /home/ubuntu/.ssh/authorized_keys
+sudo chown ubuntu:ubuntu -R /home/ubuntu/
